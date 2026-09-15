6672b008346f0cf9c939a9d505f2d6b06dc3e73e
elprofesoriqo <igorjankowwski@gmail.com>
2026-09-15 18:31:15 +0200

    add f cool flowers

--- js/components.js
+++ js/components.js
@@ -29,6 +29,28 @@ async function loadComponents() {
         if (footRes.ok) {
             document.getElementById('footer-placeholder').innerHTML = await footRes.text();
         }
+        
+        const flowersHTML = `
+            <div class="fixed top-[20%] right-[5%] pointer-events-none z-0 mix-blend-multiply opacity-60 rotate-[-10deg]">
+                <img src="assets/flower.jpg" alt="" class="w-32 md:w-48 object-contain">
+            </div>
+            <div class="fixed top-[60%] left-[3%] pointer-events-none z-0 mix-blend-multiply opacity-50 rotate-12">
+                <img src="assets/flower2.jpg" alt="" class="w-24 md:w-40 object-contain">
+            </div>
+            <div class="fixed top-[10%] left-[25%] pointer-events-none z-0 mix-blend-multiply opacity-40 rotate-[35deg]">
+                <img src="assets/flower3.jpg" alt="" class="w-20 md:w-32 object-contain">
+            </div>
+            <div class="fixed bottom-[15%] right-[25%] pointer-events-none z-0 mix-blend-multiply opacity-45 rotate-[-45deg]">
+                <img src="assets/flower4.jpg" alt="" class="w-28 md:w-44 object-contain">
+            </div>
+            <div class="fixed top-[45%] right-[20%] pointer-events-none z-0 mix-blend-multiply opacity-50 rotate-45">
+                <img src="assets/flower5.jpg" alt="" class="w-24 md:w-36 object-contain">
+            </div>
+            <div class="fixed bottom-[5%] left-[40%] pointer-events-none z-0 mix-blend-multiply opacity-30 rotate-[-15deg]">
+                <img src="assets/flower6.jpg" alt="" class="w-20 md:w-32 object-contain">
+            </div>
+        `;
+        document.body.insertAdjacentHTML('afterbegin', flowersHTML);
     } catch (e) {
         console.error('Error', e);
     }
